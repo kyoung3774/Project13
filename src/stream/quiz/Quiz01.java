fa93ebbd6a2b7c5d6b93f8bb59136e08a280fc88
BaeJooHwan <kyoung3774@naver.com>
2024-08-03 14:36:12 +0900

    Home

--- src/stream/quiz/Quiz01.java
+++ src/stream/quiz/Quiz01.java
@@ -13,20 +13,14 @@ public class Quiz01 {
 		String[] arr = { "aaa", "aaa", "b", "b", "ccccc" };
 
 		// 1. 문자열 목록중에서 길이가 2보다 큰 요소의 개수를 구하시오.(답:3)
-		Stream<String> stream1 = Stream.of(arr);
-		System.out.println(stream1.filter(s -> s.length() > 2).count());
+		Stream<String> stream1 = Arrays.stream(arr);
+		long count = stream1.filter(s -> s.length() > 2).count();
+		System.out.println("길이가 3보다 큰 요소의 개수: " + count);
 		
 		// 2. 모든 문자열의 길이를 더한 합을 구하시오.(답:13)
-		// 2-1 기본 for문 사용
-		int sum = 0;
-		for(String str : arr) {
-			sum += str.length();
-		}
-		
-		System.out.println(sum);
-		
-		// 2-2 배열 -> 스트림 변환
-
+		Stream<String> stream2 = Arrays.stream(arr);
+		int sum = stream2.mapToInt(s -> s.length()).sum();
+		System.out.println("모든 문자열의 길이를 더한 합: " + sum);
 		
 		// 3. 가장 길이가 짧은 문자열의 길이를 구하시오.(답:1)
 		Stream<String> stream4 = Arrays.stream(arr);
